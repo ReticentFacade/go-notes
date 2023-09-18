fc5ab620063edc5fdea845225d2f10126d2aa11f
Reticent Facade <lipisharma2911@gmail.com>
2023-09-18 15:18:13 +0530

    shortened path in main_dynamic.go

--- rust-plus-golang/main_dynamic.go
+++ rust-plus-golang/main_dynamic.go
@@ -1,11 +1,9 @@
 package main
 
-// #cgo CFLAGS: -I/Users/Reticent/Desktop/Work/ProjectWorkspace/github/go-notes/rust-plus-golang/lib
-// #cgo LDFLAGS: -L/Users/Reticent/Desktop/Work/ProjectWorkspace/github/go-notes/rust-plus-golang/lib/hello/target/release -lhello
+// #cgo CFLAGS: -I./lib
+// #cgo LDFLAGS: -L./lib/hello/target/release -lhello
 // #include "hello.h"
 import "C"
-
-
 
 func main() {
 	C.hello(C.CString("world"))
